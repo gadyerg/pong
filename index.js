59ee7b020483746277a6afc31844d5881b875b36
gadyerg <gadyergonzalez@gmail.com>
2023-01-10 18:19:51 -0700

    setup project added scoreboard prototype

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+const ejsmate = require('ejs-mate')
+const path = require('path')
+
+const dummy_data = [500, 300, 235, 123, 45]
+
+app.engine('ejs', ejsmate)
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.get('/', (req, res) => {
+  res.render('leaderboard', {scores: dummy_data})
+})
+
+app.listen(5000, () => {
+  console.log("listening")
+})
